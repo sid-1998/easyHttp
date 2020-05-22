cf7cd9cc852f094716060c08ca652a10ff7bfc08
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2020-05-22 16:20:17 +0530

    fetch version done
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- easyHttpFetchVersion.js
+++ easyHttpFetchVersion.js
@@ -24,4 +24,32 @@ class EasyHttp{
             .catch(err=>reject(err));
         })
     }
+
+    put(url, data){
+        return new Promise((resolve, reject)=>{
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res=>res.json())
+            .then(data=>resolve(data))
+            .catch(err=>reject(err));
+        })
+    }
+    delete(url){
+        return new Promise((resolve, reject)=>{
+            fetch(url,{
+                 method: 'DELETE',
+                 headers: {
+                     'Content-type': 'application/json'
+                 }
+            })
+            .then(res=>res.json())
+            .then(()=>resolve("Deleted"))
+            .catch(err=>reject(err));
+        })
+    }
 }
